6ba86dca1d5815b7fc43fbfdde8c5d793a097389
Aditya Pranjal <srivastavatubbu@gmail.com>
2026-02-26 14:19:27 +0530

    Bump version to 1.0.0b3 for PyPI release

--- ses_intelligence/__init__.py
+++ ses_intelligence/__init__.py
@@ -21,7 +21,7 @@ Quick Start:
 For more details, see the documentation.
 """
 
-__version__ = "1.0.0b2"
+__version__ = "1.0.0b3"
 
 # Public API exports
 from .tracing import trace_behavior, get_edge_features
