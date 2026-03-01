2d47c40cd5b4fd91417df34df37a072600d202de
Aditya Pranjal <srivastavatubbu@gmail.com>
2026-03-01 22:09:50 +0530

    Release stable version 1.1.0 with node feature extraction and health engine improvements

--- ses_intelligence/__init__.py
+++ ses_intelligence/__init__.py
@@ -21,7 +21,7 @@ Quick Start:
 For more details, see the documentation.
 """
 
-__version__ = "1.1.0rc1"
+__version__ = "1.1.0"
 
 # Public API exports
 from .tracing import trace_behavior, get_edge_features
